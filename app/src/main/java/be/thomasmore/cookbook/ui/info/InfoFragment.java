3c9ee497994fc36aec9258fb9d10d19b9fca5455
aswevers <amber.swevers@hotmail.com>
2019-12-03 19:39:09 +0100

    Info-page DONE

--- app/src/main/java/be/thomasmore/cookbook/ui/info/InfoFragment.java
+++ app/src/main/java/be/thomasmore/cookbook/ui/info/InfoFragment.java
@@ -1,6 +1,7 @@
 package be.thomasmore.cookbook.ui.info;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,8 +24,10 @@ public class InfoFragment extends Fragment {
         infoViewModel =
                 ViewModelProviders.of(this).get(InfoViewModel.class);
         View root = inflater.inflate(R.layout.fragment_info, container, false);
+        final TextView infoGithubLinks = root.findViewById(R.id.info_links);
+        infoGithubLinks.setMovementMethod(LinkMovementMethod.getInstance());
         final TextView textView = root.findViewById(R.id.text_slideshow);
-
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
         return root;
     }
 }
