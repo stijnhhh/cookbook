50c9e3d028cee57874dfaf3abebd4892178de555
aswevers <amber.swevers@hotmail.com>
2019-12-05 15:43:21 +0100

    Category added to LocalStorage DB

--- app/src/main/java/be/thomasmore/cookbook/ui/addRecipe/addRecipeFragment.java
+++ app/src/main/java/be/thomasmore/cookbook/ui/addRecipe/addRecipeFragment.java
@@ -5,8 +5,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,8 +18,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import be.thomasmore.cookbook.R;
 import be.thomasmore.cookbook.ui.helpers.DatabaseHelper;
+import be.thomasmore.cookbook.ui.models.Category;
 import be.thomasmore.cookbook.ui.models.Recipe;
 
 public class addRecipeFragment extends Fragment {
@@ -27,6 +33,7 @@ public class addRecipeFragment extends Fragment {
     private AddRecipeViewModel addRecipeViewModel;
 
     private Button addRecipeBtn;
+    private Spinner spinnerCategories;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -45,6 +52,8 @@ public class addRecipeFragment extends Fragment {
         });
 
         db = new DatabaseHelper(getActivity());
+
+        addCategoriesToSpinner();
         return root;
     }
 
@@ -60,5 +69,24 @@ public class addRecipeFragment extends Fragment {
         Toast.makeText(getActivity(),db.getRecipes() + "",Toast.LENGTH_SHORT).show();
     }
 
+    public void addCategoriesToSpinner(){
+        spinnerCategories = (Spinner) root.findViewById(R.id.categoryspinner);
+        List<String> list = new ArrayList<String>();
+
+        //db.getCategories();
+
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        /*for(Category category : categories){
+            list.add(category.toString());
+
+        }*/
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategories.setAdapter(dataAdapter);
+    }
 
 }
--- app/src/main/java/be/thomasmore/cookbook/ui/helpers/DatabaseHelper.java
+++ app/src/main/java/be/thomasmore/cookbook/ui/helpers/DatabaseHelper.java
@@ -9,6 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import java.util.ArrayList;
 import java.util.List;
 
+import be.thomasmore.cookbook.ui.models.Category;
 import be.thomasmore.cookbook.ui.models.Recipe;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -25,44 +26,41 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
     }
 
     // methode wordt uitgevoerd als de database gecreëerd wordt
     // hierin de tables creëren en opvullen met gegevens
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE_RECIPE = "CREATE TABLE recipe (" +
-                "id INTEGER PRIMARY KEY," +
+
+        String CREATE_TABLE_CATEGORY= "CREATE TABLE category (" +
+                "categoryId INTEGER PRIMARY KEY," +
                 "name TEXT)";
+        db.execSQL(CREATE_TABLE_CATEGORY);
+
+        String CREATE_TABLE_RECIPE = "CREATE TABLE recipe (" +
+                "recipeId INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT," +
+                "categoryId INTEGER," +
+                "FOREIGN KEY (categoryId) REFERENCES category(categoryId))";
         db.execSQL(CREATE_TABLE_RECIPE);
 
-        /*String CREATE_TABLE_CATEGORY= "CREATE TABLE category (" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "name TEXT," +
-                "FOREIGN KEY (partyId) REFERENCES party(id))";
-        db.execSQL(CREATE_TABLE_CATEGORY);
-*/
+        insertCategories(db);
         insertRecipes(db);
     }
 
     private void insertRecipes(SQLiteDatabase db) {
-        db.execSQL("INSERT INTO recipe (recipeId, name) VALUES (1, 'Apple pie');");
-        db.execSQL("INSERT INTO recipe (recipeId, name) VALUES (2, 'Spaghetti bolognese');");
+        db.execSQL("INSERT INTO recipe (name, categoryId) VALUES ('Apple pie', 1);");
+        db.execSQL("INSERT INTO recipe (name, categoryId) VALUES ('Spaghetti bolognese', 2);");
     }
 
-    private void insertPresidents(SQLiteDatabase db) {
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Roosevelt, Franklin Delano', '1933-1945', 2);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Truman, Harry ', '1945-1953', 2);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Eisenhower, Dwight David', '1953-1961', 1);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Kennedy, John Fitzgerald', '1961-1963', 2);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Johnson, Lyndon Baines ', '1963-1969', 2);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Nixon, Richard Milhous ', '1969-1974', 1);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Ford, Gerald Rudolph ', '1974-1977', 1);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Carter, James Earl Jr.', '1977-1981', 2);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Reagan, Ronald Wilson', '1981-1989', 1);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Bush, George Herbert Walker ', '1989-1993', 1);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Clinton, William Jefferson ', '1993-2001', 2);");
-        db.execSQL("INSERT INTO president (name, term, partyId) VALUES ('Bush, George Walker', '2001-2009', 1);");
+    private void insertCategories(SQLiteDatabase db) {
+        db.execSQL("INSERT INTO category (categoryId, name) VALUES (1, 'Dessert');");
+        db.execSQL("INSERT INTO category (categoryId, name) VALUES (2, 'Pasta');");
+        db.execSQL("INSERT INTO category (categoryId, name) VALUES (3, 'Vegetarian');");
+        db.execSQL("INSERT INTO category (categoryId, name) VALUES (4, 'Seafood');");
+        db.execSQL("INSERT INTO category (categoryId, name) VALUES (5, 'Breakfast');");
 
     }
 
@@ -70,8 +68,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // hierin de vorige tabellen wegdoen en opnieuw creëren
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS category");
         db.execSQL("DROP TABLE IF EXISTS recipe");
-        //db.execSQL("DROP TABLE IF EXISTS party");
 
         // Create tables again
         onCreate(db);
@@ -87,6 +85,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         ContentValues values = new ContentValues();
         values.put("name", recipe.getName());
+        values.put("categoryId", 1);
 
         long id = db.insert("recipe", null, values);
 
@@ -105,7 +104,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         if (cursor.moveToFirst()) {
             do {
                 Recipe recipe = new Recipe(cursor.getLong(0),
-                        cursor.getString(1), cursor.getLong(3));
+                        cursor.getString(1), cursor.getLong(2));
                 lijst.add(recipe);
             } while (cursor.moveToNext());
         }
@@ -115,4 +114,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return lijst;
     }
 
+    public List<Category> getCategories() {
+        List<Category> lijst = new ArrayList<Category>();
+
+        String selectQuery = "SELECT  * FROM category";
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Category category = new Category(cursor.getLong(0),
+                        cursor.getString(1));
+                lijst.add(category);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return lijst;
+    }
 }
