330a4f9e7f95f66c508ae742fa0c19e86b8057d7
stijnhhh <38656091+stijnhhh@users.noreply.github.com>
2019-12-19 20:11:49 +0100

    search and add to favorite

--- app/src/main/java/be/thomasmore/cookbook/ui/recipe/PlatformAdapter.java
+++ app/src/main/java/be/thomasmore/cookbook/ui/recipe/PlatformAdapter.java
@@ -6,12 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import be.thomasmore.cookbook.R;
+import be.thomasmore.cookbook.ui.helpers.DatabaseHelper;
+import be.thomasmore.cookbook.ui.helpers.DownloadImageTask;
+import be.thomasmore.cookbook.ui.models.Favorite;
 import be.thomasmore.cookbook.ui.models.RecipeAPI;
 
 public class PlatformAdapter extends ArrayAdapter<RecipeAPI> {
@@ -31,13 +36,40 @@ public class PlatformAdapter extends ArrayAdapter<RecipeAPI> {
         View rowView = inflater.inflate(R.layout.platformlistviewitem, parent, false);
 
         final TextView textName = (TextView) rowView.findViewById(R.id.name);
-        final TextView textCat = (TextView) rowView.findViewById(R.id.category);
         final ImageView mealImage = (ImageView) rowView.findViewById(R.id.meal_image);
+        final Button addToFavoriteButton = (Button) rowView.findViewById(R.id.add_to_favorite);
 
         textName.setText(values.get(position).getName());
-        textCat.setText(values.get(position).getCategory());
-        mealImage.setImageURI(Uri.parse(values.get(position).getPicture()));
+
+        new DownloadImageTask(mealImage).execute(values.get(position).getPicture());
+        mealImage.getLayoutParams().width = 200;
+
+        addToFavoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseHelper db = new DatabaseHelper(getContext());
+                int recipeID =  values.get(position).getRecipeId();
+                String recipe = values.get(position).getName();
+
+                try {
+                    db.getRecipe(recipeID);
+
+                    toon(recipe + " is already added!!");
+                } catch (Exception e) {
+                    db.insertFavorite(recipeID);
+
+                    toon(recipe + " is added to favorites!!");
+                }
+
+                toon(recipe);
+            }
+        });
 
         return rowView;
+    }
+
+    public void toon(String tekst)
+    {
+        Toast.makeText(getContext(),tekst + " added to favorites!",Toast.LENGTH_SHORT).show();
     }
 }
